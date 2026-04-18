720c93e64f1508db0e35b62a4b0cc7722c63e298
Rithesh <ritheshasowda@gmail.com>
2026-04-18 15:07:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,24 @@
 import streamlit as st
-import os
-from dotenv import load_dotenv
-from utils import load_and_process, create_vector_store, create_qa_chain
+import requests
 
-load_dotenv()
-
-st.set_page_config(page_title="RAG Assistant", layout="wide")
+st.set_page_config(page_title="RAG Assistant")
 
 st.title(" LLM RAG Assistant")
-st.write("Upload a PDF and ask questions from it!")
 
-uploaded_file = st.file_uploader("Upload your PDF", type="pdf")
+# Upload PDF
+uploaded_file = st.file_uploader("Upload PDF", type="pdf")
 
 if uploaded_file:
-    with open("temp.pdf", "wb") as f:
-        f.write(uploaded_file.read())
+    files = {"file": uploaded_file.getvalue()}
+    res = requests.post("http://127.0.0.1:8000/upload", files=files)
+    st.success(res.json()["message"])
 
-    with st.spinner("Processing document..."):
-        chunks = load_and_process("temp.pdf")
-        db = create_vector_store(chunks)
-        qa = create_qa_chain(db)
+# Ask Question
+query = st.text_input("Ask a question")
 
-    st.success("Document processed successfully!")
-
-    query = st.text_input("Ask a question")
-
-    if query:
-        with st.spinner("Generating answer..."):
-            response = qa.run(query)
-
-        st.write("###  Answer:")
-        st.write(response)
+if st.button("Get Answer"):
+    response = requests.post(
+        "http://127.0.0.1:8000/ask",
+        json={"question": query}
+    )
+    st.write(response.json()["answer"])
